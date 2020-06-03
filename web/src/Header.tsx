86ba3a20b7a987df09064a5a006e98b4be1e664e
Cristiane Faria <cristiane.trc@gmail.com>
2020-06-03 17:24:44 -0300

    Initial concepts

---
+++ web/src/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+export default Header;
+
+// FC Componente em modo de função
